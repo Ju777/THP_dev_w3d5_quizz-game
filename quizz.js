751e44b0ab60fc535cffc3d3d43b83c67ab94d7b
Julien Armagnac <julien.tamil@gmail.com>
2022-07-22 11:41:15 +0200

    Clean design.

--- quizz.js
+++ quizz.js
@@ -58,9 +58,11 @@ displayQuestion = async (quizz, nextQuestion) => {
     console.log(nextQuestion);
 
     // La div de la nouvelle question
+    if(nextQuestion.type === "multiple"){
     nextQuestionContainer.innerHTML = `
-        <h3>Question n° ${quizz.indexOf(nextQuestion) + 1}<br/>
-        ${nextQuestion.question}</h3>
+        <h3 class="mb-4 text-center">QUESTION n° ${quizz.indexOf(nextQuestion) + 1}
+        </h3>
+        <h4 class="mb-3 text-center">${nextQuestion.question}</h4>
         <div id="answers-container" class="answers-container">
             <div class="answer-container p-3 d-flex"><h5 class="mx-2">A</h5><p id="answer-a" >${nextQuestion.correct_answer}</p></div>
             <div class="answer-container p-3 d-flex"><h5 class="mx-2">B</h5><p id="answer-b" >${nextQuestion.incorrect_answers[0]}</p></div>
@@ -68,6 +70,15 @@ displayQuestion = async (quizz, nextQuestion) => {
             <div class="answer-container p-3 d-flex"><h5 class="mx-2">D</h5><p id="answer-d" >${nextQuestion.incorrect_answers[2]}</p></div>
         </div>
             `;
+        } else if (nextQuestion.type === "boolean") {
+        nextQuestionContainer.innerHTML = `
+        <h3 class="mb-4 text-center">QUESTION n° ${quizz.indexOf(nextQuestion) + 1}<br/>
+        ${nextQuestion.question}</h3>
+        <div id="answers-container" class="answers-container">
+            <div class="answer-container p-3 d-flex"><h5 class="mx-2">A</h5><p id="answer-a" >${nextQuestion.correct_answer}</p></div>
+            <div class="answer-container p-3 d-flex"><h5 class="mx-2">B</h5><p id="answer-b" >${nextQuestion.incorrect_answers[0]}</p></div>
+        </div>`
+        }
     nextQuestionContainer.style.display = 'block';
     correctAnswers.push(nextQuestion.correct_answer);
     // FIN
@@ -76,22 +87,20 @@ displayQuestion = async (quizz, nextQuestion) => {
     anime({
         targets: nextQuestionContainer,
         translateX: "25vw",  
-        // rotate : 360,
         duration : 2000,
         direction : "normal"
-        // autoplay : false,
     });
     // FIN
 
-    // On met un écouteur d'évènement au mousehover sur chaque case de réponse, pour modifier le pointeur de la souris.
     const answerContainers = document.querySelectorAll('.answer-container');
 
     answerContainers.forEach(container => {
+        // On met un écouteur d'évènement au mousehover sur chaque case de réponse, pour modifier le pointeur de la souris.
         container.addEventListener('mouseover', () => {
             container.style.cursor = 'pointer';
-            // console.log("OVER AND OVER");
         });
 
+        // On met un écouteur d'évènement au click sur chaque case de réponse, pour passer à la prochaine question et enregistrer la réponse de l'utlisateur.
         container.addEventListener('click', () => {
             console.log('clicked');
             console.log(container.childNodes);
@@ -153,7 +162,7 @@ displayQuestion = async (quizz, nextQuestion) => {
 }
 
 announceGameResults = () => {
-    // On efface la div qio posent les questions, on n'en aura plus besoin.
+    // On efface la div qui posent les questions, on n'en aura plus besoin.
     nextQuestionContainer.style.display = 'none';
 
     // Calcul des bonnes réponses
@@ -163,42 +172,65 @@ announceGameResults = () => {
     // On affiche les réponses du joueur en regard des bonnes réponses.
     const resultsContainer = document.createElement('div');
     resultsContainer.setAttribute('id', 'results-container');
-    resultsContainer.style.display = 'flex';
+    // resultsContainer.style.display = 'flex';
     mainContainer.appendChild(resultsContainer);
-    resultsContainer.innerHTML = `<p>
-                                    Es-tu le grand gagnant ?<br/>
-                                    Tu as ${nbCorrectAnswers} bonnes réponses !</p>
-                                 <div>
-                                    <ul id="player-answers">
-                                    </ul>
-                                 </div>
-                                    
-                                 <div>
-                                    <ul id="correct-answers">
-                                    </ul>
-                                 </div>
-    
-                                 `;
-    
+    resultsContainer.innerHTML = `  <div class="score-announce">
+                                        <h1 class="text-danger">
+                                             SCORE ${nbCorrectAnswers} / ${correctAnswers.length}
+                                        </h1>
+                                    </div>
+                                    <div class="answers-lists">
+                                        <div>
+                                            <ul id="questions-title">
+                                                <li class="bg-info mb-3">QUESTIONS</li>
+                                            </ul>
+                                        </div>                                           
+                                        <div>
+                                            <ul id="correct-answers">
+                                            <li class="bg-info mb-3">CORRECTION</li>
+                                            </ul>
+                                        </div>
+                                        <div>
+                                            <ul id="player-answers">
+                                            <li class="bg-info mb-3">JOUEUR</li>
+                                            </ul>
+                                        </div>
+                                    </div>`;
 
-    // Affichage des réponses du joueur  
+    // Affichage des réponses du joueur
+    const questionsUl = document.getElementById('questions-title');
     const playerUl = document.getElementById('player-answers');
-    for(let i = 0 ; i < playerAnswers.length ; i++){
-        const li = document.createElement('li');
-        playerUl.appendChild(li);
-        li.innerHTML = playerAnswers[i];
-    }
+    // for(let i = 0 ; i < playerAnswers.length ; i++){
+    //     const li = document.createElement('li');
+    //     playerUl.appendChild(li);
+    //     li.innerHTML = playerAnswers[i];
+    // }
 
     const correctUl = document.getElementById('correct-answers');
     for(let i = 0 ; i < correctAnswers.length ; i++){
-        const li = document.createElement('li');
-        correctUl.appendChild(li);
-        li.innerHTML = correctAnswers[i];
+        const liQuestion = document.createElement('li');
+        questionsUl.appendChild(liQuestion);
+        liQuestion.innerHTML = `Q${i}`;
+
+        const liPlayer = document.createElement('li');
+        playerUl.appendChild(liPlayer);
+        liPlayer.innerHTML = playerAnswers[i];
+
+        const liCorrect = document.createElement('li');
+        correctUl.appendChild(liCorrect);
+        liCorrect.innerHTML = correctAnswers[i];
+
+        if (correctAnswers[i] === playerAnswers[i]) {
+            liPlayer.style.backgroundColor = "green";
+        }
+         else {
+            liPlayer.style.backgroundColor = "red";
+        }
     }
 
     const replayButton = document.createElement('button');
     mainContainer.appendChild(replayButton);
-    replayButton.innerHTML = "REJOUER ?";
+    replayButton.innerHTML = "<h1>REPLAY ?</h2>";
     replayButton.addEventListener('click', () => {
         document.location.reload(true);
     });
@@ -213,8 +245,6 @@ computeResults = (playerAnswers, correctAnswers) => {
     }
     return count;
 }
-
-
 
 perform = () => {
     console.clear();
